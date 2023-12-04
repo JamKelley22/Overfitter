3489abc9722918efae97adf92a8c8a09f9774b1b
Jameel Kelley <jamkelley22@gmail.com>
2023-12-03 19:36:43 -0600

    Refreshing project

--- src/routes/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import { error } from '@sveltejs/kit';
-import prisma from '$lib/prisma';
-import type { PageServerLoad } from './$types';
-
-export const load = (async () => {
-    const response = await prisma.accessory.findMany()
-    return { feed: response };
-}) satisfies PageServerLoad;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,2 @@
-<script lang="ts">
-	import Counter from './Counter.svelte';
-
-	export let data;
-</script>
-
-<svelte:head>
-	<title>Overfitter</title>
-	<meta name="A closet management system for the home" content="Overfitter demo app" />
-</svelte:head>
-
-<section>
-	{#each data.feed as { id, name, description }}
-		<li><a href="#">{name}</a></li>
-	{/each}
-</section>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
